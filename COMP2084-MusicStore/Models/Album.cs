90849987052ee9c06cc4ea8af0eefa3a80382299
Mitchell Haak <200329261@student.georgianc.on.ca>
2019-09-30 16:55:35 -0400

    Add project files.

---
+++ COMP2084-MusicStore/Models/Album.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2084_MusicStore.Models
+{
+    public class Album
+    {
+        [Key]
+
+        public int AlbumId { get; set; }
+
+        [ForeignKey("Genre")]
+
+        public int GenreId { get; set; }
+
+        public String Name { get; set; }
+
+        public String Artist { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ COMP2084-MusicStore/Models/Song.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2084_MusicStore.Models
+{
+    public class Song
+    {
+        [Key]
+        public int SongId { get; set; }
+
+        [ForeignKey("Album")]
+        public int AlbumId { get; set; }
+
+        public String SongName { get; set; }
+
+        public String ArtistName { get; set; }
+
+        public String Featuring { get; set; }
+
+        public int RunTimeSeconds { get; set; }
+
+    }
+}
